68b0e58a46d6c65e1886daaf7bffae33a3180865
Kyle <kyle.c.r.fahey@gmail.com>
2016-11-21 20:31:31 -0600

    Mongo

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ config.db = {};
 
 config.webhost = 'http://localhost:3000/';
 
-config.db.host = 'master:1234@ds159767.mlab.com:59767/';
+config.db.host = 'master:1234@ds159767.mlab.com:59767';
 config.db.name = 'kylely';
 
 module.exports = config;
